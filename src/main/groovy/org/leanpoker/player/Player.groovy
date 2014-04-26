e97abfa54da9156a01ffe77b223346d265b45c8c
katona.zsolt <katona.zsolt.mail@gmail.com>
2014-04-26 12:31:21 +0200

    version 0.4

--- src/main/groovy/org/leanpoker/player/Player.groovy
+++ src/main/groovy/org/leanpoker/player/Player.groovy
@@ -2,7 +2,7 @@ package org.leanpoker.player
 
 class Player {
 
-    static final String VERSION = 'Great and Grandious Groovy 0.3'
+    static final String VERSION = 'Great and Grandious Groovy 0.4'
 
 	static GameStateHelper helper
 
