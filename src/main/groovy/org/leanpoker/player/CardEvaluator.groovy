0e27d92cf8b6beeac0d8fba035c407217c99a310
Bence Kiglics <bence.kiglics@gmail.com>
2014-04-26 10:32:17 +0200

    added rule for big cards

--- src/main/groovy/org/leanpoker/player/CardEvaluator.groovy
+++ src/main/groovy/org/leanpoker/player/CardEvaluator.groovy
@@ -1,13 +1,16 @@
 package org.leanpoker.player
 
 class CardEvaluator {
-    def cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    static final def CARDS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
     static def evaluate(List<Object> holeCards) {
         def points = 0;
 
         if (holeCards[0].rank == holeCards[1].rank)
-            points += 2;
+            points += 5;
+
+        if (CARDS.indexOf(holeCards[0].rank) > 6 || CARDS.indexOf(holeCards[1].rank) > 6)
+            points += 1;
 
         points
     }
