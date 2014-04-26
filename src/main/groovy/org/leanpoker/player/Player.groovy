6e5082b4e6c2deba155fe2dfe58a34db5ce4f611
katona.zsolt <katona.zsolt.mail@gmail.com>
2014-04-26 11:53:32 +0200

    refined betting and evaluating

--- src/main/groovy/org/leanpoker/player/Player.groovy
+++ src/main/groovy/org/leanpoker/player/Player.groovy
@@ -2,33 +2,32 @@ package org.leanpoker.player
 
 class Player {
 
-    static final String VERSION = 'Great and Grandious Groovy 0.2'
+    static final String VERSION = 'Great and Grandious Groovy 0.3'
 
 	static GameStateHelper helper
 
     static int betRequest(def gameState) {
 		helper = new GameStateHelper(gameState: gameState)
 
-		return 1000
-
-		// if the bet is too high, we must tread cautiously
-		if (helper.us.stack > 50) {
-			if (helper.callAmount > helper.us.stack / 2) {
-				return 0 // we fold, or call
-			}
-			if (helper.callAmount > helper.us.stack / 3) {
-				return helper.callAmount // we call
-			}
-		}
-
 		// if the stakes are right, we bet as normal
 		switch (CardEvaluator.evaluate(helper.us.hole_cards)) {
 			case 0:
 				return 0
 			case 1..2:
-				return helper.callAmount
+				// if the bet is too high, we must tread cautiously
+				if (helper.us.stack > 50) {
+					if (helper.callAmount > helper.us.stack / 2) {
+						return 0 // we fold, or call
+					}
+					if (helper.callAmount > helper.us.stack / 3) {
+						return helper.callAmount // we call
+					}
+				} else {
+					return helper.callAmount
+				}
 			case 3..10:
-				return helper.callAmount + helper.minimumRaise
+				return helper.us.stack
+//				return helper.callAmount + helper.minimumRaise
 		}
     }
 
