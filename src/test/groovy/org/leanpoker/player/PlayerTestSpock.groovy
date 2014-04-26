2d22b705c4c900a959fdbf89bf4d2df6f670485b
Bence Kiglics <bence.kiglics@gmail.com>
2014-04-26 15:19:04 +0200

    added random test to ensure we're not failing

--- src/test/groovy/org/leanpoker/player/PlayerTestSpock.groovy
+++ src/test/groovy/org/leanpoker/player/PlayerTestSpock.groovy
@@ -15,4 +15,41 @@ class PlayerTestSpock extends Specification {
 		expect:
 		Player.betRequest(gameState) >= 0
 	}
+
+	def "random cards does not fail"() {
+		given:
+		def gameState = new JsonSlurper().parseText(new File("src/test/resources/testdata.json").text)
+        def helper = new GameStateHelper(gameState: gameState)
+        def rand = new Random()
+
+		when:
+        (1..100000).each {
+            helper.us.hole_cards = [getRandomCard(), getRandomCard()]
+            helper.us.stack = rand.nextInt(1000)
+            helper.us.bet = rand.nextInt(1000)
+
+            gameState.minimum_raise = rand.nextInt(1000)
+            gameState.current_buy_in = rand.nextInt(1000)
+            gameState.community_cards = []
+
+            if (rand.nextFloat() > 0.5) {
+                (3..4 + rand.nextInt(3)).each {
+                    gameState.community_cards += getRandomCard()
+                }
+            }
+
+            Player.betRequest(gameState)
+        }
+
+        then:
+        notThrown(Exception)
+	}
+
+    def getRandomCard() {
+        def suits = ["hearts", "diamonds", "spades", "clubs"]
+        def ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+        ['rank': ranks[new Random().nextInt(ranks.size())],
+         'suit': suits[new Random().nextInt(suits.size())]]
+    }
 }
